fad583501718f108c28ecbf195ac2e9af297efd0
Dan Roesch <danielar2143@gmail.com>
2021-03-21 20:31:53 -0500

    adding list functionality

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'Flash' },
+    {id: 2, name: 'BatMan'},
+    {id: 3, name: 'Robin'},
+    {id: 4, name: 'Wonderwoman'},
+    {id: 5, name: 'SuperMan'},
+    {id: 6, name: 'Godzilla'},
+    {id: 7, name: 'Mothman'},
+    {id: 8, name: 'Zeuz'},
+    {id: 9, name: 'Geroge'},
+    {id: 10, name: 'Cornflake Homunculus'}
+];
